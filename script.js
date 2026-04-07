ba5acf2f94d2ed540aa01aa8921f8fa673976066
Mrs. Celniker <68243452+dicelniker@users.noreply.github.com>
2026-04-07 20:54:01 +0000

    Play and Buttons

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+<<<<<<< HEAD
 //Game State
+=======
+// --- Game State ---
+>>>>>>> 6177152 (Play and Buttons)
 let answer = 0;
 let guessCount = 0;
 let totalWins = 0;
 let totalGuesses = 0;
+<<<<<<< HEAD
 let scores = 0;
 
 //Player Name
@@ -24,3 +29,36 @@ document.getElementById("playBtn").addEventListener("click", function(){
 answer = Math.floor(Math.random() * range) + 1;
 
 document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range;
+=======
+let scores = [];
+
+
+// --- Player Name ---
+let playerName = prompt("Enter your name:");
+
+// --- Play ---
+document.getElementById("playBtn").addEventListener("click", function () {
+  let radios = document.getElementsByName("level");
+  let range = 3;
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      range = parseInt(radios[i].value);
+    }
+  }
+  //pick answer
+
+    answer = Math.floor(Math.random() * range) + 1;
+    // Disable & Enable buttons and radio choices
+    document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range;
+    document.getElementById("guess").value="";
+    document.getElementById("guessBtn").disabled = false;
+    document.getElementById("giveUpBtn").disabled = false;
+    document.getElementById("playBtn").disabled = true;
+
+    let levelRadios = document.getElementsByName("level");
+    for (let i = 0; i < levelRadios.length; i++) {
+        levelRadios[i].disabled = true;
+    }
+
+});
+>>>>>>> 6177152 (Play and Buttons)
