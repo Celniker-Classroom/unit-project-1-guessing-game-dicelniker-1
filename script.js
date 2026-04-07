6a165dc987c6d384545ba98f5df79d4de413c99f
Mrs. Celniker <68243452+dicelniker@users.noreply.github.com>
2026-04-07 21:24:48 +0000

    update radio levels variable for consistency

--- script.js
+++ script.js
@@ -1,37 +1,9 @@
-<<<<<<< HEAD
-//Game State
-=======
 // --- Game State ---
->>>>>>> 6177152 (Play and Buttons)
 let answer = 0;
 let guessCount = 0;
 let totalWins = 0;
 let totalGuesses = 0;
-<<<<<<< HEAD
-let scores = 0;
-
-//Player Name
-let playerName = prompt("Enter your name:");
-
-//Play
-//get level
-document.getElementById("playBtn").addEventListener("click", function(){
-    let radios = document.getElementsByName("level");
-    let range = 3;
-    for (let i=0; i < radios.length; i++){
-        if(radios[i].checked){
-            range = parseint(radios[i].value);
-        }
-    }
-})
-
-//round setup
-answer = Math.floor(Math.random() * range) + 1;
-
-document.getElementById("msg").textContent = playerName + ", guess a number between 1 and " + range;
-=======
 let scores = [];
-
 
 // --- Player Name ---
 let playerName = prompt("Enter your name:");
@@ -55,10 +27,9 @@ document.getElementById("playBtn").addEventListener("click", function () {
     document.getElementById("giveUpBtn").disabled = false;
     document.getElementById("playBtn").disabled = true;
 
-    let levelRadios = document.getElementsByName("level");
-    for (let i = 0; i < levelRadios.length; i++) {
-        levelRadios[i].disabled = true;
+    //disable radio level selection
+    for (let i = 0; i < radios.length; i++) {
+        radios[i].disabled = true;
     }
 
 });
->>>>>>> 6177152 (Play and Buttons)
